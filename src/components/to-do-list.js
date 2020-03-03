b842256d62370f1e2ad088e21491e142256a8da1
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-03 16:39:23 +0300

    More styles stuff modified

--- src/components/to-do-list.js
+++ src/components/to-do-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import TodoListItem from './to-do-list-item'
+import TodoListItem from './to-do-list-item';
+import './to-do-list.css';
 
 const TodoList = ({todos}) => {
 
@@ -7,14 +8,14 @@ const TodoList = ({todos}) => {
 
         const {id, ...itemProps} = item;
         return (
-        <li key={id}>
+        <li key={id} className="list-group-item">
             <TodoListItem {...itemProps}/>
             </li>
         );
     });
 
     return (
-        <ul>
+        <ul className="list-group todo-list">
             {elements}
         </ul>
     );
