b4f90ba7a8366fe72dec7831ab109a5ac8def413
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-03-12 12:40:59 +0300

    Next part done

--- src/components/to-do-list-item/to-do-list-item.js
+++ src/components/to-do-list-item/to-do-list-item.js
@@ -5,7 +5,8 @@ import './to-do-list-item.css';
 export default class TodoListItem extends Component {
 
 state = {
-    done: false
+    done: false,
+    important: false
   };
 
 onLabelClick = () => {
@@ -14,32 +15,37 @@ onLabelClick = () => {
       });
 };
 
+onMarkImportant = () => {
+  this.setState({
+    important: true
+  });
+};
+
 
   render() {
 
-    const { label, important = false } = this.props;
-    const {done} = this.state;
+    const { label} = this.props;
+    const {done, important} = this.state;
     let classNames = 'to-do-list-item';
     if (done) {
       classNames += ' done';
     };
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    if (important) {
+      classNames += 'important';
+    }
   
     return (
       <span className={classNames}>
         <span
           className="to-do-list-item-label"
-          style={style}
           onClick = {this.onLabelClick}>
           {label}
         </span>
   
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={this.onMarkImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
